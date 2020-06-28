bdcef7154b9cd2b48a208a762ce5d6be1b7f1a1b
Matthew Eng <matthew.eng2@gmail.com>
2020-06-28 11:09:11 -0400

    Fill in variables

--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    url: 'mattheweng', // the auth0 domain prefix
+    audience: 'grind_coffee', // the audience set for the auth0 app
+    clientId: 'V5KT70wDOXwdT8JsJcc2gJzSAwuElgg6', // the client id generated for the auth0 app
+    callbackURL: 'http://localhost:8100', // the base url of the running ionic application.
   }
 };
