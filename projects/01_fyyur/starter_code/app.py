b368457f97e0f24e841e69a7d83114b763dbe65b
Matthew Eng <matthew.eng2@gmail.com>
2020-07-12 21:10:33 -0400

    test

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -195,6 +195,7 @@ def search_venues():
   # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for Hop should return "The Musical Hop".
   # search for "Music" should return "The Musical Hop" and "Park Square Live Music & Coffee"
+
   response={
     "count": 1,
     "data": [{
